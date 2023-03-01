0d3d1940d035b31d9e3ccb41d023c5e8d0342f66
mnanke <c11421nm@gmail.com>
2023-03-01 19:01:09 +0900

    need fix

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mnanke <mnanke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 13:19:17 by mnanke            #+#    #+#             */
-/*   Updated: 2023/03/01 17:37:54 by mnanke           ###   ########.fr       */
+/*   Updated: 2023/03/01 18:38:09 by mnanke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,19 @@ int	distr_af_pc(const char input, va_list *ap)
 		return (ft_putchar(va_arg(*ap, int)));
 	else if (input == 's')
 		return (ft_putstr(va_arg(*ap, char *)));
-	// else if (input == 'p')
-	// 	return (ft_putstr("0x")
-	// 		+ ft_putnbr_sixteen(va_arg(*ap, unsigned long int), '0'));
-	// else if (input == 'd' || input == 'i')
-	// 	return (ft_putnbr(va_arg(*ap, int)));
-	// else if (input == 'u')
-	// 	return (ft_putnbr_unsigned(va_arg(*ap, unsigned int)));
-	// else if (input == 'x')
-	// 	return (ft_putnbr_sixteen(va_arg(*ap, unsigned int), '0'));
-	// else if (input == 'X')
-	// 	return (ft_putnbr_sixteen(va_arg(*ap, unsigned int), '1'));
-	// else if (input == '%')
-	// 	return (ft_putchar('%'));
+	else if (input == 'p')
+		return (ft_putstr("0x")
+			+ ft_putnbr_sixteen(va_arg(*ap, unsigned long int), "0"));
+	else if (input == 'd' || input == 'i')
+		return (ft_putnbr(va_arg(*ap, int)));
+	else if (input == 'u')
+		return (ft_putnbr_unsigned(va_arg(*ap, unsigned int)));
+	else if (input == 'x')
+		return (ft_putnbr_sixteen(va_arg(*ap, unsigned int), "0"));
+	else if (input == 'X')
+		return (ft_putnbr_sixteen(va_arg(*ap, unsigned int), "1"));
+	else if (input == '%')
+		return (ft_putchar('%'));
 	return (0);
 }
 
@@ -66,20 +66,20 @@ int	main(void)
 	strcpy(putp, "abcd");
 	printf("printf(c):%c\n", putc);
 	ft_printf("ft_printf(c):%c\n--------------\n", putc);
-	// printf("printf(s):%s\n", putp);
-	// ft_printf("ft_printf(s):%s\n--------------\n", putp);
-// 	printf("printf:%p\n", putp);
-// 	ft_printf("ft_printf:%p\n--------------\n", putp);
-// 	printf("printf:%d\n", INT_MIN);
-// 	ft_printf("ft_printf:%d\n--------------\n", INT_MIN);
-// 	printf("printf:%i\n", INT_MIN);
-// 	ft_printf("ft_printf:%i\n--------------\n", INT_MIN);
-// 	printf("printf:%u\n", UINT_MAX);
-// 	ft_printf("ft_printf:%u\n--------------\n", UINT_MAX);
-// 	printf("printf:%x\n", UINT_MAX);
-// 	ft_printf("ft_printf:%x\n--------------\n", UINT_MAX);
-// 	printf("printf:%X\n", UINT_MAX);
-// 	ft_printf("ft_printf:%X\n--------------\n", UINT_MAX);
-// 	printf("printf:%%\n");
-// 	ft_printf("ft_printf:%%\n--------------\n");
+	printf("printf(s):%s\n", putp);
+	ft_printf("ft_printf(s):%s\n--------------\n", putp);
+	printf("printf(p):%p\n", putp);
+	ft_printf("ft_printf(p):%p\n--------------\n", putp);
+	printf("printf(d):%d\n", INT_MIN);
+	ft_printf("ft_printf(d):%d\n--------------\n", INT_MIN);
+	printf("printf(i):%i\n", INT_MIN);
+	ft_printf("ft_printf(i):%i\n--------------\n", INT_MIN);
+	printf("printf(u):%u\n", UINT_MAX);
+	ft_printf("ft_printf(u):%u\n--------------\n", UINT_MAX);
+	printf("printf(x):%x\n", UINT_MAX);
+	ft_printf("ft_printf(x):%x\n--------------\n", UINT_MAX);
+	printf("printf(X):%X\n", UINT_MAX);
+	ft_printf("ft_printf(X):%X\n--------------\n", UINT_MAX);
+	printf("printf(pc):%%\n");
+	ft_printf("ft_printf(pc):%%\n--------------\n");
 }
